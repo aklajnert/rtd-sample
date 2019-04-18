8d7ee4f533d5a2a3075c6186bb99c77ee335ed13
Andrzej Klajnert <github@aklajnert.pl>
2019-04-18 15:39:05 +0200

    Initial

---
+++ docs/_build/html/_static/readthedocs-data.js
@@ -0,0 +1,19 @@
+var READTHEDOCS_DATA = {
+    project: "rtd-admin-demo",
+    version: "latest",
+    language: "en",
+    programming_language: "words",
+    subprojects: {},
+    canonical_url: "http://localhost:9000/docs/rtd-admin-demo/en/latest/",
+    theme: "sphinx_rtd_theme",
+    builder: "sphinx",
+    docroot: "/docs/",
+    source_suffix: ".rst",
+    api_host: "http://127.0.0.1:8000",
+    commit: "291410ee",
+    ad_free: false,
+
+    global_analytics_code: 'None',
+    user_analytics_code: null
+};
+
